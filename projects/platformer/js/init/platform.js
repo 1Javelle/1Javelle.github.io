4e507962f71cfc7c7ef9303d50b3e988f4e233d1
1Javelle <javelle.brown.student@madisonpsb.org>
2021-10-21 15:42:09 +0000

    platformer

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,7 +39,7 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
-        createPlatform(225,150);
+        createPlatform(225,120);
         createPlatform()
         createPlatform()
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
